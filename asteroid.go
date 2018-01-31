b961b833c351c4a1d861e9aa13447f1401841a54
Angad Nadkarni <angadn@Angads-Hullo-MacBook-Pro.local>
2018-01-31 15:13:28 +0530

    Added a test-case

--- asteroid.go
+++ asteroid.go
@@ -16,7 +16,7 @@ type SIPHeaderWatch struct {
 	cb           func(callID string, headers map[string]string)
 
 	// Internals
-	readCloser io.ReadCloser
+	reader io.Reader
 }
 
 // NewSIPHeaderWatch for a set of headers to watch for, and a callback to run when a call
@@ -33,52 +33,60 @@ func NewSIPHeaderWatch(
 	return ret
 }
 
-// SetAsteriskPath to the Asterisk executable
+// SetAsteriskPath to the Asterisk executable.
 func (watch *SIPHeaderWatch) SetAsteriskPath(path string) {
 	watch.asteriskPath = path
 }
 
+// SetReader instead of Asterisk executable.
+func (watch *SIPHeaderWatch) SetReader(reader io.Reader) {
+	watch.reader = reader
+}
+
 // Start watching for headers.
 func (watch *SIPHeaderWatch) Start() {
-	cmd := exec.Command(watch.asteriskPath, "-r")
-
 	var err error
-	if err = cmd.Run(); err == nil {
-		if watch.readCloser, err = cmd.StdoutPipe(); err == nil {
-			go func() {
-				s := bufio.NewScanner(watch.readCloser)
-				var (
-					callID       string
-					headerValues map[string]string
-				)
+	if watch.reader == nil {
+		cmd := exec.Command(watch.asteriskPath, "-r")
+		if err = cmd.Run(); err == nil {
+			watch.reader, err = cmd.StdoutPipe()
+		}
+	}
 
-				headerValues = make(map[string]string)
-				for s.Scan() {
-					line := s.Text()
-					if len(line) > 0 {
-						if len(callID) == 0 {
-							fmt.Sscanf(line, "Call-ID: %s@", &callID)
-						} else {
-							for _, key := range watch.headers {
-								var val string
-								if fmt.Sscanf(line, key+": %s", &val); len(val) > 0 {
-									headerValues[key] = val
-									break
-								}
+	if err == nil {
+		go func() {
+			s := bufio.NewScanner(watch.reader)
+			var (
+				callID       string
+				headerValues map[string]string
+			)
+
+			headerValues = make(map[string]string)
+			for s.Scan() {
+				line := s.Text()
+				if len(line) > 0 {
+					if len(callID) == 0 {
+						fmt.Sscanf(line, "Call-ID: %s", &callID)
+					} else {
+						for _, key := range watch.headers {
+							var val string
+							if fmt.Sscanf(line, key+": %s", &val); len(val) > 0 {
+								headerValues[key] = val
+								break
 							}
 						}
-					} else {
-						if len(callID) > 0 {
-							// Fire callback
-							watch.cb(callID, headerValues)
+					}
+				} else {
+					if len(callID) > 0 {
+						// Fire callback
+						watch.cb(callID, headerValues)
 
-							// Reset our state
-							callID = ""
-							headerValues = make(map[string]string)
-						}
+						// Reset our state
+						callID = ""
+						headerValues = make(map[string]string)
 					}
 				}
-			}()
-		}
+			}
+		}()
 	}
 }
